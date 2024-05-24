20517f6d92b3d3a7a3ffafffb7531bf541472ae7
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2024-05-24 13:39:30 +0200

    Added macOS listener object with TODOs

---
+++ src/platform/macos/NDLListener.h
@@ -0,0 +1,32 @@
+/*
+ * NDL - Native Dark mode Listener.
+ *
+ * Copyright (C) 2024  mhahnFr
+ *
+ * This file is part of NDL.
+ *
+ * NDL is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NDL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with NDL.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#import <Foundation/Foundation.h>
+
+#import "../platform.h"
+
+@interface NDLListener : NSObject
+
+- (BOOL) addCallback: (ndl_platform_callback) callback;
+- (BOOL) removeCallback: (ndl_platform_callback) callback;
+- (BOOL) empty;
+
+@end
